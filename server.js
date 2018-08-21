0a5dcc7ff66b7cc06ce14b9d56d2f52e95b99155
paulinhojpma <paulinhosocrates@gmail.com>
2018-08-21 10:28:26 -0300

    terceiro commit

--- server.js
+++ server.js
@@ -8,15 +8,131 @@ var uuid = require('uuid');
 
 var jwt    = require('jsonwebtoken'); 
 var config = require('./config'); 
-var User   = require('./user'); 
 
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
-var credentials = new AWS.SharedIniFileCredentials({profile: 'SQS profiles'});
-AWS.config.credentials = credentials;
-AWS.config.update({region: 'us-west-2'});
+var credentials = AWS.config.loadFromPath('./config.json');
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-//var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+
+
+var params = {
+ AttributeNames: [
+    "SentTimestamp"
+ ],
+ MaxNumberOfMessages: 1,
+ MessageAttributeNames: [
+    "All"
+ ],
+ QueueUrl: "https://sqs.us-east-2.amazonaws.com/700728690443/Lista.fifo",
+ VisibilityTimeout: 20,
+ WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, function(err, data) {
+  if (err) {
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+  	console.log("Mensagens recebidas body- "+ data+ data);
+  	console.log("Mensagens recebidas body- "+ data.body+ data.);
+  	console.log("Mensagens recebidas id- "+ data.MessageId+ data.Body);
+    var deleteParams = {
+      QueueUrl: "https://sqs.us-east-2.amazonaws.com/700728690443/Lista.fifo",
+      ReceiptHandle: data.Messages[0].ReceiptHandle
+    };
+    sqs.deleteMessage(deleteParams, function(err, data) {
+      if (err) {
+        console.log("Delete Error", err);
+      } else {
+        console.log("Message Deleted", data);
+      }
+    });
+  }
+});
+
+
+
+/*var params = {
+ 
+ MessageAttributes: {
+  "Title": {
+    DataType: "String",
+    StringValue: "The Whistler"
+   },
+  "Author": {
+    DataType: "String",
+    StringValue: "John Grisham"
+   },
+  "WeeksOn": {
+    DataType: "Number",
+    StringValue: "6"
+   }
+ },
+ MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+ MessageGroupId: "vicentinho090909",
+ MessageDeduplicationId: "vicentinho090909111111",
+
+ QueueUrl: "https://sqs.us-east-2.amazonaws.com/700728690443/Lista.fifo"
+};
+
+sqs.sendMessage(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.MessageId);
+  }
+});*/
+
+/*var params = {
+  QueueUrl: 'https://sqs.us-east-2.amazonaws.com/700728690443/Lista.fifo'
+ };
+
+sqs.deleteQueue(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});*/
+
+
+
+/*var params = {
+  QueueName: 'Lista.fifo'
+};
+
+sqs.getQueueUrl(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.QueueUrl);
+  }
+});*/
+
+
+
+/*var params = {
+  QueueName: 'lista',
+  Attributes: {
+    'DelaySeconds': '60',
+    'MessageRetentionPeriod': '86400'
+  }
+};
+
+sqs.createQueue(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.QueueUrl);
+  }
+});*/
+
+/*var params= {};
+sqs.listQueues(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.QueueUrls);
+  }
+});*/
 
 
 var port = process.env.PORT || 8090; 
